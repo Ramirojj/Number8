585c6d07e655640be4cafde5c64f7b667bac8541
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2025-02-11 13:50:56 -0600

    [2.09] Create balloons.html / attach bootstrap

--- discount.js
+++ discount.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('discount-row').addEventListener('click', function(e){
-        e.preventDefault();
-
-    bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+        if (e.target.classList.contains('discount')) {
+            e.preventDefault();
+            document.getElementById('product').innerHTML = e.target.dataset['product'];
+            bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+          }
 });
 });
